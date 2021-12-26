68f58c4f30af25613e0c12b7d4e1a313dc04276a
tatsuro1997 <minagawa@dptr.jp>
2021-12-26 13:30:05 +0900

    shop edit, update

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,11 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
+
 
 class ShopController extends Controller
 {
@@ -42,12 +47,39 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        dd(Shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        // dd(Shop::findOrFail($id));
+
+        return view('owner.shops.edit', compact('shop'));
 
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
 
+        $request->validate([
+            'name' => ['required', 'string', 'max:50'],
+            'information' => ['required', 'string', 'max:1000'],
+            'is_selling' => ['required'],
+        ]);
+
+        $imageFile = $request->image;
+        if(!is_null($imageFile) && $imageFile->isValid()){
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
+        }
+
+        $shop = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->information = $request->information;
+        $shop->is_selling = $request->is_selling;
+        if(!is_null($imageFile) && $imageFile->isValid()){
+            $shop->filename = $fileNameToStore;
+        }
+
+        $shop->save();
+
+        return redirect()
+        ->route('owner.shops.index')
+        ->with(['message' => '店舗情報を更新しました。', 'status' => 'info']);
     }
 }
--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -9,8 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <x-flash-message status="session('status')" />
                     @foreach ($shops as $shop)
-                        <div class="w-1/2 p-4"
+                        <div class="w-1/2 p-4">
                             <a href="{{ route('owner.shops.edit', ['shop' => $shop->id]) }}">
                                 <div class="border rounded-md p-4">
                                     <div class="mb-4">
@@ -21,13 +22,7 @@
                                         @endif
                                     </div>
                                     <div class="text-x1">{{ $shop->name }}</div>
-                                    <div>
-                                        @if(empty($shop->filename))
-                                            <img src="{{ asset('images/no_image.jpg') }}">
-                                        @else
-                                            <img src="{{ asset('strage/shops/' . $shop->filename) }}">
-                                        @endif
-                                    </div>
+                                    <x-shop-thumbnail :filename="$shop->filename" />
                                 </div>
                             </a>
                         </div>
