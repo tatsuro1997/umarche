5ad2e966a8f9368d02ccfeda3c70fd3e9a2db6a8
tatsuro1997 <minagawa@dptr.jp>
2021-12-29 11:32:03 +0900

    micromodal

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,11 @@ require('./bootstrap');
 
 import Alpine from 'alpinejs';
 
+import MicroModal from 'micromodal';  // es6 module
+MicroModal.init({
+  disableScroll: true
+});
+
 window.Alpine = Alpine;
 
 Alpine.start();
