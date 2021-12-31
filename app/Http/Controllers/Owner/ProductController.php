43e77e750a6ee6135d467ef5f5f0a8ddb2630752
tatsuro1997 <minagawa@dptr.jp>
2021-12-31 09:10:38 +0900

    constant

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -172,10 +172,10 @@ class ProductController extends Controller
                     $product->is_selling = $request->is_selling;
                     $product->save();
 
-                    if($request->type == '1'){
+                    if($request->type == \Constant::PRODUCT_LIST['add']){
                         $newQuamtity = $request->quantity;
                     }
-                    if ($request->type == '2') {
+                    if ($request->type == \Constant::PRODUCT_LIST['reduce']) {
                         $newQuamtity = $request->quantity * -1;
                     }
                     Stock::create([
